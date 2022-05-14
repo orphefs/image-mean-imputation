e86eb1d399951b2cf922b7632602ad480af644cb
Orfeas Kypris <kypriso@gmail.com>
2022-05-14 11:52:56 +0300

    fix no_boundary_4x4_blob test case

--- test/test_impute_image.py
+++ test/test_impute_image.py
@@ -1,6 +1,7 @@
 import pytest
 import numpy as np
 from pytest_lazyfixture import lazy_fixture
+
 from src.main import impute_image
 
 
@@ -31,6 +32,7 @@ def no_boundary_2x2_blob():
                                          [1., 1., 1., 1., 1., 1., 1., 1., 1.]])
     return image, calibration_image, expected_corrected_image
 
+
 @pytest.fixture
 def no_boundary_3x3_blob():
     calibration_image = np.array([
@@ -58,11 +60,39 @@ def no_boundary_3x3_blob():
     return image, calibration_image, expected_corrected_image
 
 
+@pytest.fixture
+def no_boundary_4x4_blob():
+    calibration_image = np.array([
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, -1, -1, -1, -1, 0, 0, 0],
+        [0, 0, -1, -1, -1, -1, 0, 0, 0],
+        [0, 0, -1, -1, -1, -1, 0, 0, 0],
+        [0, 0, -1, -1, -1, -1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ])
+
+    image = np.array([[1., 1., 1., 1., 1., 1., 1., 1., 1.],
+                      [1., 1., 2., 13., 4., 100., 1., 1., 1.],
+                      [1., 1., 3., 2., 5., 2., 1., 1., 1.],
+                      [1., 1., 6., 15., 9., 9., 1., 1., 1.],
+                      [1., 1., 5., 6., 7., 9., 1., 1., 1.],
+                      [1., 1., 1., 1., 1., 1., 1., 1., 1.]])
+
+    expected_corrected_image = np.array([[1., 1., 1., 1., 1., 1., 1., 1., 1.],
+                                         [1., 1., 1., 1., 1., 1., 1., 1., 1.],
+                                         [1., 1., 1., 1., 1., 1., 1., 1., 1.],
+                                         [1., 1., 1., 1., 1., 1., 1., 1., 1.],
+                                         [1., 1., 1., 1., 1., 1., 1., 1., 1.],
+                                         [1., 1., 1., 1., 1., 1., 1., 1., 1.]])
+    return image, calibration_image, expected_corrected_image
+
+
 @pytest.mark.parametrize("data", [
     lazy_fixture("no_boundary_2x2_blob"),
     lazy_fixture("no_boundary_3x3_blob"),
+    lazy_fixture("no_boundary_4x4_blob"),
 
-                                  ])
+])
 def test_impute_image(data):
     result = impute_image(image=data[0], calibration_image=data[1])
     print(result)
