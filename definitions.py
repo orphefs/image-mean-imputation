df29f6bfc0d5ae0d23086b1990ccd795fb84666e
Orfeas Kypris <kypriso@gmail.com>
2022-05-15 19:59:53 +0300

    add dtypes

--- definitions.py
+++ definitions.py
@@ -1,5 +1,12 @@
 from pathlib import Path
 import os
+import numpy as np
 
+# dirs
 ROOT_DIR = Path(os.path.dirname(__file__))
 DATA_DIR = ROOT_DIR / "data"
+
+# dtypes
+calibration_image_dtype = np.float32
+image_dtype = np.uint16
+
